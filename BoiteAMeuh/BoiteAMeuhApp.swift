ee130b2bee796635b94c43040add9e555f633a88
Philippe MICHEL <philippe.michel1@gmail.com>
2020-12-13 16:44:22 +0100

    Initial Commit

---
+++ BoiteAMeuh/BoiteAMeuhApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoiteAMeuhApp.swift
+//  BoiteAMeuh
+//
+//  Created by Philippe MICHEL on 13/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct BoiteAMeuhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoiteAMeuh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BoiteAMeuh
+//
+//  Created by Philippe MICHEL on 13/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
